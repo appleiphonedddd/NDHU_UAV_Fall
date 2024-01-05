d61cfe7c2e162c0111b632d7dc022e6c5758ebe4
Egor Kreed <89340462+appleiphonedddd@users.noreply.github.com>
2024-01-05 15:47:01 +0800

    Update camera_avaliable.py

--- Final_Project/camera_avaliable.py
+++ Final_Project/camera_avaliable.py
@@ -4,11 +4,15 @@ def list_available_cameras(max_index):
     available_cameras = []
     for i in range(max_index):
         cap = cv2.VideoCapture(i)
+        
+        # If camera can be open
         if cap.isOpened():
-            available_cameras.append(i)
-            cap.release()
+            available_cameras.append(i) # Append to the array
+            cap.release()               # Release camera then handle next
     return available_cameras
 
 max_camera_index = 10
 available_cameras = list_available_cameras(max_camera_index)
+
+# Print available camera on PC
 print("Available Cameras:", available_cameras)
